aa8259d6b14cb1206134ebe3c6a5f8ab2f27e738
Ume099 <bykawa099@gmail.com>
2024-10-13 14:26:43 +0900

    firebase-admin用の設定を追加

--- next.config.js
+++ next.config.js
@@ -8,4 +8,18 @@ module.exports = {
       },
     ],
   },
+  webpack: (config) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+    };
+    config.resolve.fallback = {
+      fs: false,
+      http2: false,
+      events: false,
+      stream: false,
+      util: false,
+    };
+    return config;
+  },
 };
