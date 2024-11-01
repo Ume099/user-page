b0b50b33a7656611107aabe88b1728b313f49f39
Ume099 <bykawa099@gmail.com>
2024-11-02 00:43:53 +0900

    パスワードリセット画面を実装

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -79,6 +79,7 @@ export const Page = () => {
               </button>
             </div>
           </form>
+          <p>ログインできない場合は<a href="https://www.alt-prime.com/resetPassword">こちら</a></p>
         </div>
       ) : (
         <PageListAfterSignIn linkList={linkList} />
