ab9d7a78d94a13d000280868aeda094420d8aa45
Ume099 <bykawa099@gmail.com>
2024-11-03 11:52:00 +0900

    指導報告書のオリジナル記載欄を追加

--- src/lib/teachingReport.ts
+++ src/lib/teachingReport.ts
@@ -10,7 +10,11 @@ export type TeachingReportTemplateInputType = {
   rikaido: string;
   comment: string;
   isPublished: boolean;
-  isOriginal: boolean;
+  isOriginalStage: boolean;
+  stageName?: string;
+  isOriginalTopic: boolean;
+  toopic?: string;
+  isOriginalBehav: boolean;
 };
 
 export type TeachingReportTemplateInputPostType = {
