6b9e218727e2918541d40599a96a6c4c33912e76
Ume099 <bykawa099@gmail.com>
2024-12-06 20:15:56 +0900

    google search consoleの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,20 +10,29 @@ import Header from '@/components/common/Header';
 import SideBar from '@/components/common/SideBar';
 import { AuthProvider } from '@/feature/auth/provider/AuthProvider';
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import HeaderInfo from '@/components/common/HeaderInfo';
 
 initializeFirebaseApp();
+
+const googleVerificationContent = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <GoogleTagManager googleTagManagerId={googleTagManagerId} />
       <RecoilRoot>
+        <Head>
+          <title>EXAMPLE TITLE</title>
+          <meta name="description" content="EXAMPLE DESCRIPTION" />
+          <meta
+            name="google-site-verification"
+            content={googleVerificationContent || 'default-verification-code'}
+          />
+        </Head>
         <ChakraProvider>
           <AuthProvider>
             <Header />
-            <HeaderInfo />
             <SideBar />
             <Component {...pageProps} />
             <MessageDialog />
--- src/utils/gtm.ts
+++ src/utils/gtm.ts
@@ -1,5 +1,5 @@
 export type GoogleTagManagerId = `GTM-${string}`;
 
 export const googleTagManagerId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
-  ? (process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as GoogleTagManagerId)
+  ? (process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE_CONTENT as GoogleTagManagerId)
   : undefined;
